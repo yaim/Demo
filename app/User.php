816f85777b3ee2125d6c4445e77a4effdf33c162
Yashar Imanlou <yashari@gmail.com>
2020-06-03 03:17:16 +0000

    Setup tymon/jwt-auth JWT Provider

--- app/User.php
+++ app/User.php
@@ -6,8 +6,9 @@ use App\LoginAttempt;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -38,8 +39,19 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     public function successfulLoginAttempts()
     {
         return $this->hasMany(LoginAttempt::class);
     }
+
 }

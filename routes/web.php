7a1dbcc2f92b2909c491c822f4d8f15c40731eef
Yashar Imanlou <yashari@gmail.com>
2020-06-03 04:18:53 +0000

    Remove Laravel's default Auth API & View Scaffolding

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::any('/{any}', function () {
 	return view('app');
 })->where('any', '.*');
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
